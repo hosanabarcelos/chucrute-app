e2283cee006ab87c2800af4b29689d0998adfda2
Hosana <hosanabarceloscandido@gmail.com>
2022-08-24 13:29:40 -0300

    feat: create button default component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,47 @@
+<template>
+    <router-link :to="path">
+        <button class="button">{{ text }}</button>
+    </router-link>
+</template>
+
+<script>
+
+export default {
+    name: 'PrimaryButton',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        path: {
+            type: String,
+            default: '',
+        },
+        text: {
+            type: String,
+            default: '',
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.button {
+    font-size: .9rem;
+    border-radius: .3rem;
+    padding: .8rem 1.2rem;
+    border: none;
+    font-weight: 500;
+    color: #FFF;
+    background-color: #6857db;
+    cursor: pointer;
+    transition-duration: 0.4s;
+}
+.button:hover {
+    opacity: .9;
+}
+
+</style>
+
