65b8886c0ba78c23747f10b9b787cab0a86baf1d
Hosana <hosanabarceloscandido@gmail.com>
2022-10-10 18:46:21 -0300

    feat: add script para bot

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>Chat</h1>
+    <div>
+        <h1>Chat</h1>
+        <iframe src="https://web.powerva.microsoft.com/environments/Default-a70b6cf1-294d-4251-9456-c01e5624032b/bots/new_bot_8088a2608a50494e8d2c31c4bb2d2c1f/webchat" frameborder="0" style="width: 50%; height: 550px;"></iframe>
+    </div>
 </template>
+
+<style scoped>
+
+</style>
